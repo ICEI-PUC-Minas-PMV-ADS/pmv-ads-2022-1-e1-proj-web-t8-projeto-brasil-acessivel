24bc8eb5124e87aefa6a723136b94915dd23eae4
Mike Possidonio <maike.silva.17@gmail.com>
2022-05-31 12:18:53 -0300

    correção do arquivo banco de dados para json

--- src/bancodados.js
+++ src/bancodados.js
@@ -1,61 +1,74 @@
-var bd = {
-    dados: [
+var bd = `{
+    "restaurantes": [
         {
-            imagem: 'http://source.unsplash.com/200x180/?restaurant',
-            nome: 'Restaurante Bom Prato',
-            categoria: 'Bares e Restaurantes',
-            acessibilidade: 'Rampa - Cardapio em braile - Área Kids',
-            nota: 4.7,
-            informacoes: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis nam minus nisi error fuga totam!'
-        }, {
-            imagem: 'http://source.unsplash.com/200x180/?hotel/1',
-            nome: 'Hotel Caminho da Montanha',
-            categoria: 'Hotéis',
-            acessibilidade: 'Elevador - Quarto adaptado',
-            nota: 3.9,
-            informacoes: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis id nihil libero ea mollitia repellendus a tenetur atque iure doloribus.'
-        }, {
-            imagem: 'http://source.unsplash.com/200x180/?clinic/1',
-            nome: 'Clincia Ortopedica',
-            categoria: 'Clinica Médica',
-            acessibilidade: 'Libras - rampa',
-            nota: 4.2,
-            informacoes: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, unde quod hic doloremque sint quae eaque odit laudantium aspernatur temporibus, dolorem enim.'
-        }, {
-            imagem: 'http://source.unsplash.com/200x180/?store/1',
-            nome: 'Loja Veste Bem',
-            categoria: 'Loja de Roupa',
-            acessibilidade: 'Libras',
-            nota: 4.8,
-            informacoes: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea adipisci dolorum nam.'
-        }, {
+            "imagem": "http://source.unsplash.com/200x180/?restaurant",
+            "nome": "Restaurante Bom Prato",
+            "categoria": "Bares e Restaurantes",
+            "acessibilidade": "Rampa - Cardapio em braile - Área Kids",
+            "nota": 4.7,
+            "informacoes": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis nam minus nisi error fuga totam!"
+        }, 
+{
             imagem: 'http://source.unsplash.com/200x180/?bar',
             nome: 'Bar Copo Sujo',
             categoria: 'Bares e Restaurantes',
             acessibilidade: 'Banheiro Adaptado - Cardapio em braile',
             nota: 3.7,
             informacoes: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, unde quod hic doloremque sint quae eaque odit laudantium aspernatur temporibus, dolorem enim.'
-        }, {
+ }
+];
+"hoteis": [
+{
+            imagem: 'http://source.unsplash.com/200x180/?hotel/1',
+            nome: 'Hotel Caminho da Montanha',
+            categoria: 'Hotéis',
+            acessibilidade: 'Elevador - Quarto adaptado',
+            nota: 3.9,
+            informacoes: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis id nihil libero ea mollitia repellendus a tenetur atque iure doloribus.'
+        },
+{
             imagem: 'http://source.unsplash.com/200x180/?hotel/2',
             nome: 'Hotel Sol da Manhã',
             categoria: 'Hotéis',
             acessibilidade: 'Libras - Informações em braile',
             nota: 4.6,
             informacoes: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea adipisci dolorum nam.'
-        }, {
+}
+];
+ "clinicas": [
+{
+            imagem: 'http://source.unsplash.com/200x180/?clinic/1',
+            nome: 'Clincia Ortopedica',
+            categoria: 'Clinica Médica',
+            acessibilidade: 'Libras - rampa',
+            nota: 4.2,
+            informacoes: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, unde quod hic doloremque sint quae eaque odit laudantium aspernatur temporibus, dolorem enim.'
+        },
+{
             imagem: 'http://source.unsplash.com/200x180/?clinic/2',
             nome: 'Centro Médico Especializado',
             categoria: 'Clinica Médica',
             acessibilidade: 'Rampa - Espaço Kids',
             nota: 3.7,
             informacoes: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis nam minus nisi error fuga totam!'
-        }, {
+}
+];
+"lojas": [ 
+{
+            imagem: 'http://source.unsplash.com/200x180/?store/1',
+            nome: 'Loja Veste Bem',
+            categoria: 'Loja de Roupa',
+            acessibilidade: 'Libras',
+            nota: 4.8,
+            informacoes: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea adipisci dolorum nam.'
+        },
+{
             imagem: 'http://source.unsplash.com/200x180/?store/2',
             nome: 'Calça Apertada Jeans',
             categoria: 'Loja de Roupa',
             acessibilidade: 'Rampa',
             nota: 2.8,
             informacoes: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis id nihil libero ea mollitia repellendus a tenetur atque iure doloribus.'
-        },
-    ]
 }
+   ]
+}`;
