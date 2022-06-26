d80d02d05dcf581030f178f714a8976c6bcb2434
fernends <90225435+fernends@users.noreply.github.com>
2022-06-25 21:24:19 -0300

    Formatação
    Ajustar a formatação do código e alterar as informações das linhas 28 e 30 para letras maiúsculas na primeira letra das palavras

--- src/js/login.js
+++ src/js/login.js
@@ -1,33 +1,32 @@
-        var emeio = document.getElementById("email");
-        var seguranca = document.getElementById("senha");
+var emeio = document.getElementById("email");
+var seguranca = document.getElementById("senha");
 var envia = document.getElementById("enviar");
 var usuario = JSON.parse(bd).usuarios;
 var conteudoHtml = "";
 var erro;
 envia.onclick = registrado;
-function registrado()   {
+function registrado() {
     let section = document.getElementById("logado");
-conteudoHtml = "";
-section.innerHTML = "";
-    for(var contador = 0; contador < usuario.length; contador++) {
-let dUsuario = usuario[contador];
-if(emeio.value == dUsuario.email && seguranca.value == dUsuario.senha) {
-    conteudoHtml += "<h3>Usuário: "+dUsuario.nome+"</h3>";
-conteudoHtml += "<br>";
-conteudoHtml += "<span>e-mail: "+dUsuario.email+"</span>";
-conteudoHtml += "<br>";
-conteudoHtml += "<span>idade: "+dUsuario.idade+"</span>";
-conteudoHtml += "<br>";
-conteudoHtml += "<span>deficiência: "+dUsuario.deficiencia+"</span>";
-section.innerHTML = conteudoHtml;
-break;
-}
-}
+    conteudoHtml = "";
+    section.innerHTML = "";
+    for (var contador = 0; contador < usuario.length; contador++) {
+        let dUsuario = usuario[contador];
+        if (emeio.value == dUsuario.email && seguranca.value == dUsuario.senha) {
+            conteudoHtml += "<h3>Usuário: " + dUsuario.nome + "</h3>";
+            conteudoHtml += "<br>";
+            conteudoHtml += "<span>e-mail: " + dUsuario.email + "</span>";
+            conteudoHtml += "<br>";
+            conteudoHtml += "<span>idade: " + dUsuario.idade + "</span>";
+            conteudoHtml += "<br>";
+            conteudoHtml += "<span>deficiência: " + dUsuario.deficiencia + "</span>";
+            section.innerHTML = conteudoHtml;
+            break;
+        }
+    }
 
-if(contador == usuario.length) {     
-alert("usuário ou senha invalidos!");
-} else {
-    alert("usuário logado com sucesso!");
+    if (contador == usuario.length) {
+        alert("Usuário ou Senha Invalidos!");
+    } else {
+        alert("Usuário Logado com Sucesso!");
     }
 }
-    
